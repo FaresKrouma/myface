be35279211bb0437be77ecf08295650d247d2036
Fares Krouma <88767998+FaresKrouma@users.noreply.github.com>
2023-02-11 14:58:08 +0100

    moved darkModeContext to app.js from index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/authContext";
-import { DarkModeContextProvider } from "./context/darkModeContext";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <DarkModeContextProvider>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </DarkModeContextProvider>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 );
